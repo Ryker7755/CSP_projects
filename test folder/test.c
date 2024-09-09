6a27c76d8e20eb3f074f93467028c70c9e5c7018
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-09 20:24:13 +0000

    made variable c nots

--- test folder/test.c
+++ test folder/test.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(void){
-    printf("hello World!");
+    char name[] = "RykerRich," ; 
+    char nmb1n10[] = "7," ;
+    char nmb10n100[] = "200," ;
+    char breakfast[] = "eggs," ;
+    char favclr[] = "blk," ;
+    char schoolnm[] = "Ucas," ;
+    char year[] = "2024," ;
+    char eyeclr[] = "brwn," ;
+    char myAge[] = "14," ;
+    char favSbjInSchl[] = "Math" ;
+    printf("%s", name);
+    printf("%s", nmb1n10);
+    printf("%s", nmb10n100);
+    printf("%s", breakfast);
+    printf("%s", favclr);
+    printf("%s", schoolnm);
+    printf("%s", year);
+    printf("%s", eyeclr);
+    printf("%s", myAge);
+    printf("%s", favSbjInSchl);
     return 0;
 }
