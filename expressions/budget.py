16f7ecbacfd11bc7437bf668fc717deea454809d
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-15 02:38:49 +0000

    expressions fix finish

---
+++ expressions/budget.py
@@ -0,0 +1,3 @@
+float = 
+print("Hello, and welcome to your finacial calculator!\n")
+input()
--- expressions/expressions.py
+++ expressions/expressions.py
@@ -1,15 +1,19 @@
+problem1= 7-24/8*4+6
 
+problem2= 18/3-7+2*5
 
-print(7-24/8*4+6)
+problem3= 6*4/12+72/8-9
 
-print(18/3-7+2*5)
+problem4= (17-6/2)+4*3
 
-print(6*4/12+72/8-9)
+problem5= -2*(1*4-2/2)+(6+2-3)
 
-print((17-6/2)+4*3)
+problem6= -1*(3-4*7)/5-2*24/6
 
-print(-2(1*4-2/2)+(6+2-3))
+problem7= (3*5**2/15)-(5-2**2)
 
-print(-1*[(3-4*7)/5]-2*24/6)
+problem8= 1**4*2**2+3**3-2**5/4
 
-(3*5)
+problem9= (22/2-2*5)**2+(4-6/6)**2
+
+print( problem1, problem2, problem3, problem4, problem5, problem6, problem7, problem8, problem9)
