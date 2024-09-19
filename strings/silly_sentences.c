0ef816f1f1035afa809a36a3e91e02c699bda164
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-19 20:29:49 +0000

    silly sentece c

---
+++ strings/silly_sentences.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char name[20], place[20], verb[20], noun[20], sentence[500];
+    printf("type a name: ");
+    scanf("%s",name);
+    printf("type a place: ");
+    scanf("%s",place);
+    printf("type a verb: ");
+    scanf("%s", verb);
+    printf("type a noun: ");
+    scanf("%s", noun);
+    strcat(sentence, name);
+    strcat(sentence," went to the ");
+    strcat(sentence, place);
+    strcat(sentence," where they ");
+    strcat(sentence, verb);
+    strcat(sentence, " and bought a ");
+    strcat(sentence, noun);
+    strcat(sentence, ".\n");
+    printf("%s", sentence);
+    return 0;   
+}
