31f2ec9fa20ce7fda116b05a52f2e088cf776d9a
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-23 20:41:51 +0000

    finish function

---
+++ Functions/HelloWorldPt2.py
@@ -0,0 +1,13 @@
+
+def hello(name):
+    return f"hello {name}!"
+name1 = "Ryker"
+name2 = "Tyson"
+name3 = "Juni"
+name4 = "Jarod"
+name5 = "christy"
+print(hello(name1))
+print(hello(name2))
+print(hello(name3))
+print(hello(name4))
+print(hello(name5))
