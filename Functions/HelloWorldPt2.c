7f476bc66875c4fe23e1897216efa81986da145e
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-24 20:41:40 +0000

    finish c hello pt2

---
+++ Functions/HelloWorldPt2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void hello(char name[]){
+    printf("hello %s", name);
+}
+int main(void){
+    hello("Ryker ");
+    hello("Juni ""\n");
+    hello("jared ""\n");
+    hello("christy ""\n");
+    hello("Tyson ");
+    return 0;   
+}
