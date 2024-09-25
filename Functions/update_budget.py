3d3c0c98ed117e38ff1bb9e95c7095c0fdc0b44c
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-25 20:41:43 +0000

    made updated budget py

---
+++ Functions/update_budget.py
@@ -0,0 +1,28 @@
+
+def input()
+
+print("Hello, and welcome to your finacial calculator!\n")
+income = input ("how much do you make a month: \n")
+utilities = input ("how much is your monthly utilities: \n")
+rent = input ("how much is your monthly house or rent payment: \n")
+groceries = input ("how much is your monthly groceries: \n")
+transportation = input ("how much is your monthly transportation: \n")
+
+expenses = rent + utilities + groceries + transportation
+savings = income * 0.2
+total = income - expenses - savings
+prent = rent/income *100
+putilities = utilities/income *100
+pgroceries = groceries/income *100
+ptransport = transportation/income *100
+pexpenses = expenses/income *100
+
+print("your income",income)
+print("your expenses",expenses)
+print("your savings",savings)
+print("your total",total)
+print("percent of your rent",prent)
+print("percent of your utilities",putilities)
+print("percent of your groceries",pgroceries)
+print("percent of your transport",ptransport)
+print("percent of your expenses",pexpenses)
