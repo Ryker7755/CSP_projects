e79fcddbcea1a853f31b9085a5b19476f61d00b7
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-25 20:40:16 +0000

    finish budget c

--- Functions/HelloWorldPt2.c
+++ Functions/HelloWorldPt2.c
@@ -4,7 +4,7 @@ void hello(char name[]){
     printf("hello %s", name);
 }
 int main(void){
-    hello("Ryker ");
+    hello("Ryker \n");
     hello("Juni ""\n");
     hello("jared ""\n");
     hello("christy ""\n");
---
+++ Functions/update_budget.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+float income, rent, utilities, groceries, transportation, expenses, savings, total;
+
+float input(char type[], float var){
+    printf("how much is your %s: \n", type);
+    scanf("%f", &var);
+    return var;
+};
+
+void percent(char type[],int amount){
+   int per = amount/income*100;
+   printf("your %s is %d%% of your income.\n", type, per);
+}
+
+int main(void){
+    printf("This is going to calculate your budget for the month\n");
+    income = input("income", income);
+    rent = ("rent",rent);
+    utilities = input("utilities",utilities);
+    groceries = ("groceries", groceries);
+   transportation = input("transportation", transportation);
+
+    expenses = rent + utilities + groceries + transportation;
+    savings = income *.2;
+    total = income - expenses - savings;
+    printf("you make $%.2f\n", income);
+    printf("you save $%.2f\n", savings);
+    printf("you have $%.2f\n", total);
+    
+    percent("rent", rent);
+    percent("utilities", utilities);
+    percent("groceries", groceries);
+    percent("transportation", transportation);
+    percent("savings", savings);
+    percent("expenses", expenses);
+    
+    
+ return 0;   
+}
