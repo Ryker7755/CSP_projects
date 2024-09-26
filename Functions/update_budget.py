65b00153238c9cf3bccab4e206959f83aa5e829a
Ryker7755 <ryker.rich@ucas-edu.net>
2024-09-26 20:41:50 +0000

    cal py

--- Functions/update_budget.py
+++ Functions/update_budget.py
@@ -1,28 +1,32 @@
 
-def input()
+def scanf(type):
+    
+    return input(f"how much is your {type}: ")
 
 print("Hello, and welcome to your finacial calculator!\n")
-income = input ("how much do you make a month: \n")
-utilities = input ("how much is your monthly utilities: \n")
-rent = input ("how much is your monthly house or rent payment: \n")
-groceries = input ("how much is your monthly groceries: \n")
-transportation = input ("how much is your monthly transportation: \n")
+income = float (scanf("income"))
+utilities = float (scanf("utilities"))
+rent= float (scanf("rent"))
+groceries = float (scanf("groceries"))
+transportation = float (scanf("transportation"))
 
 expenses = rent + utilities + groceries + transportation
 savings = income * 0.2
 total = income - expenses - savings
-prent = rent/income *100
-putilities = utilities/income *100
-pgroceries = groceries/income *100
-ptransport = transportation/income *100
-pexpenses = expenses/income *100
+
+def percent(type,amount):
+
+    per = amount/income*100
+
+    print(f"your {type} is {per}% income")
+
 
 print("your income",income)
 print("your expenses",expenses)
 print("your savings",savings)
 print("your total",total)
-print("percent of your rent",prent)
-print("percent of your utilities",putilities)
-print("percent of your groceries",pgroceries)
-print("percent of your transport",ptransport)
-print("percent of your expenses",pexpenses)
+percent("rent",rent)
+percent("utilities",utilities)
+percent("groceries",groceries)
+percent("transport",transportation)
+percent("expenses",expenses)
