1a07ac43f897ad04520dab68136ee00354403756
Ryker7755 <ryker.rich@ucas-edu.net>
2024-10-01 20:10:35 +0000

    better old enough c

--- conditionals/conditionals.c
+++ conditionals/conditionals.c
@@ -9,15 +9,15 @@ int main(void){
 
     //figuring out what user can do 
     if (age >= 18){
-        printf("you are old enough to vote");
+        printf("you are old enough to vote\n");
     }else if (age >= 16){
-        printf("you are old enough to drive");
+        printf("you are old enough to drive\n");
     }else if (age >= 15){
-        printf("you are old enough to get learners permit");
+        printf("you are old enough to get learners permit\n");
     }else if (age >= 5){
-        printf("you are old enough to go to school");};
+        printf("you are old enough to go to school\n");};
     if (age < 5){
-        printf("you cant even go to school, you should get off this program");};
+        printf("you cant even go to school, you should get off this program\n");};
 
  return 0;   
 }
