f3fbd2ea46d8e26290778dcab22e2f5b6032956f
Ryker7755 <ryker.rich@ucas-edu.net>
2024-10-02 20:30:47 +0000

    c and py time of day

--- conditionals/time_of_day.c
+++ conditionals/time_of_day.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <time.h>
 
-int main(void){
-    
-    int time;
-    printf("what time is it: ");
-    scanf("%d", &time);
+int hour;
+
+int main(){
+    time_t now;
+    struct tm*now_tm;
+
+    now = time(NULL);
+    now_tm = localtime(&now);
+    hour = now_tm->tm_hour;
+    printf("%d\n", hour-6);
+
+    if(hour <= 12){
+        printf("Good Morning!\n");
+    } else if (hour <= 18){
+        printf("Good Afternoon\n");
+    } else if (hour <= 20){
+        printf("Good Evening!\n");
+    }else{
+        printf("GOOD NIGHT, GO TO BED!!!");
+    }
+
+
 
  return 0;   
 }
--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -1,11 +1,18 @@
-time = int(input("what is the current time without ':' and in a (24) time: \n"))
 
-if time == 24 or time >= 1 and time < 12:
-    print("good morning\n")
-elif time >= 12 and time < 18:
-    print("good afternoon\n")
-elif time <= 18 and time >= 21 or time == 21:
-    print("good evening\n")
-elif time > 21:
-    print("good night\n")
+import datetime
+
+now = datetime.datetime.now().hour
+
+if now <= 12:
+    print("Good Morning!\n")
+elif now <= 18:
+    print("Good Afternoon!\n")
+elif now <= 20:
+    print("Good Evening!\n")
+else :
+    print("Good Night!\n")
+
+
+
+
 
