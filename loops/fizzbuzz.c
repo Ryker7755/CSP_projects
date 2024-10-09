aeb5d4418ad7d1f037a912cd443c7897035c5a8c
Ryker7755 <ryker.rich@ucas-edu.net>
2024-10-09 20:41:15 +0000

    almost c fin

---
+++ loops/fizzbuzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int num;
+
+
+int main(void){
+
+    while(num<50){
+        num += 1;
+        if (num%3 ==0 && num != 0){
+            printf("Fizz");
+        }
+        else if (num%5 ==0 && num != 0){
+            printf
+        }
+    }
+ return 0;   
+}
---
+++ loops/fizzbuzz.py
@@ -0,0 +1,12 @@
+num = 0
+num += 1 
+for num in range(1,51):
+    if num%3 == 0 and num%5 == 0 and num != 0:
+        print("FizzBuzz")
+    elif num%3 == 0 and num!= 0:
+        print("Fizz")
+    elif num%5 == 0 and num!= 0:
+        print("Buzz")
+    else:
+        print(num)
+    
