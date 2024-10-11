11bc9eca16e1ffa74fc90cee95bec0227d739335
Ryker7755 <ryker.rich@ucas-edu.net>
2024-10-11 19:58:39 +0000

    fizzbuzz

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+int i, usr;  
+char one[50], two[50], three[50];
 
-int num;
+void delay(int secs){
+    int milli = 1000*secs;
+
+    clock_t start = clock();
+
+    while(clock()< start+milli);
+}
 
 
 int main(void){
+    printf("Give me a number: \n");
+    scanf("%d", &usr);
+    printf("Give me a word: \n");
+    scanf("%s", one);
+    printf("Give me another word: \n");
+    scanf("%s", two);
+    strcat(three,one);
+    strcat(three,two);
+    //Create a Loop that counts to 50
+    for(i=1;i<=usr;i++){
+       
 
-    while(num<50){
-        num += 1;
-        if (num%3 ==0 && num != 0){
-            printf("Fizz");
+        //replace #'s divisible by 3 and 5 with "FizzBuzz"
+        if (i%3==0 && i%5==0){
+            printf("%s\n",three);
+        }else if (i%3==0){
+        //replace #'s divisible by 3 with "Fizz"
+            printf("%s\n",one);
+        //replace #'s divisible by 5 with "Buzz"
+        }else if(i%5==0){
+            printf("%s\n",two);
+        }else{
+        //print all other numbers
+        printf("%d\n", i);
         }
-        else if (num%5 ==0 && num != 0){
-            printf
-        }
+        delay(30);
     }
- return 0;   
+return 0;   
 }
--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -2,11 +2,11 @@ num = 0
 num += 1 
 for num in range(1,51):
     if num%3 == 0 and num%5 == 0 and num != 0:
-        print("FizzBuzz")
+        print("FizzBuzz!")
     elif num%3 == 0 and num!= 0:
-        print("Fizz")
+        print("Fizz!")
     elif num%5 == 0 and num!= 0:
-        print("Buzz")
+        print("Buzz!")
     else:
         print(num)
     
